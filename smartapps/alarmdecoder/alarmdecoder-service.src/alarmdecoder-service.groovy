ca90c4bca16a00dc71f86dd9a2b3a5d1ab4afad2
Jason Fleisher <github@jasonfleisher.com>
2019-01-13 20:33:54 -0700

    adjust for 0-based array

--- smartapps/alarmdecoder/alarmdecoder-service.src/alarmdecoder-service.groovy
+++ smartapps/alarmdecoder/alarmdecoder-service.src/alarmdecoder-service.groovy
@@ -823,12 +823,10 @@ def addZone(evt) {
 	def sensorMap = ['10':'Front Door', '11':'Dining Room S.G.D', '12':'Garage Entry Door', '13':'Laundry Rm Bath Window', '14':'Kitchen Window', '15':'Kitchen Nook Window', '16':'Family Rm Door','17':'Master Bd Rm Window 1', '18':'Master Bd Rm Window 2', '19':'Master Bath Door', '20':'Hall Bathroom Window', '21':'Guest Room Window', '22':'Bedroom 3 Window', '23':'Bedroom 4 Window','24':'Bedroom 5 Window', '25':'Motion - Hallway'];
 	def sensorKeys = sensorMap.keySet() as String[]; 
     
-    def i = evt.value.toInteger();
-    log.info("App Event: addZone ${i}")
+    def i = evt.value.toInteger() - 1;
 	def currentSensorKey = sensorKeys[i];	
-    log.info("App Event: addZone ${i}: ${sensorKeys[i]} ${sensorMap[currentSensorKey]}")
 	def currentSensorValue = sensorMap[currentSensorKey];
-    log.info("App Event: addZone ${i}: ${currentSensorValue}")
+    log.info("App Event: addZone ${i}: ${sensorKeys[i]} ${sensorMap[currentSensorKey]}")
     try {        
         def zone_switch = addChildDevice("alarmdecoder", "AlarmDecoder virtual contact sensor", "${evt.data}", state.hub, [name: "${evt.data}", label: "${currentSensorKey}: ${currentSensorValue}", completedSetup: true])
         def sensorValue = "open"
